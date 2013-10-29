a89f9760423a99526c6a00d5ccbcb010686f4dd4
Brad Simantel <brad@theprogrampdx.com>
2013-10-29 11:23:52 -0700

    I can't remember if I made an initial commit or not

--- config/routes.js
+++ config/routes.js
@@ -14,6 +14,26 @@ module.exports = function (app, passport, auth) {
   app.get('/users/:userId', users.show)
   
   app.param('userId', users.user)
+ 
+  // league routes
+  var leagues = require('../app/controllers/leagues')  
+  app.get('/leagues', leagues.all)
+  app.post('/leagues', auth.requiresLogin, leagues.create)
+  app.get('/leagues/:leagueId', leagues.show)
+  app.put('/leagues/:leagueId', auth.requiresLogin, leagues.update)
+  app.del('/leagues/:leagueId', auth.requiresLogin, leagues.destroy)
+ 
+  app.param('leagueId', leagues.league)
+ 
+  // fantasy team routes
+  var fantasyteams = require('../app/controllers/fantasyteams')  
+  app.get('/fantasyteams', fantasyteams.all)
+  app.post('/fantasyteams', auth.requiresLogin, fantasyteams.create)
+  app.get('/fantasyteams/:fantasyTeamId', fantasyteams.show)
+  app.put('/fantasyteams/:fantasyTeamId', auth.requiresLogin, fantasyteams.update)
+  app.del('/fantasyteams/:fantasyTeamId', auth.requiresLogin, fantasyteams.destroy)
+ 
+  app.param('fantasyTeamId', fantasyteams.fantasyteam)
   
   // home route
   var index = require('../app/controllers/index')
