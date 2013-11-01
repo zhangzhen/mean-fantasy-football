087f5ceef4db3cf55ab44539a330115772d60e49
Brad Simantel <brad@theprogrampdx.com>
2013-11-01 16:07:48 -0700

    Updated players for strict search

--- public/js/controllers/players.js
+++ public/js/controllers/players.js
@@ -1,12 +1,12 @@
 window.angular.module('ngff.controllers.players', [])
   .controller('PlayersController', ['$scope', 'Global', 'NFL', 'Players',
-    function ($scope, Global, NFL) {
+    function ($scope, Global, NFL, Players) {
       $scope.global = Global;
  
       $scope.positions = NFL.positions;
       $scope.nflteams = NFL.teams;
       $scope.limitct = 10;
-
+ 
       $scope.find = function (query) {
         Players.query(query, function (players) {
           $scope.players = players;
